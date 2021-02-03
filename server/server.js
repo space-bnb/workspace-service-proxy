88074b85ecbb7a580a48b3d470ccb68650b24043
Benntobox <benjaminsboyle3@gmail.com>
2021-02-03 01:10:13 -0800

    Added nearby-workspaces service

--- server/server.js
+++ server/server.js
@@ -6,17 +6,23 @@ const path = require('path');
 app.use('/buildings/:workspaceId', express.static(__dirname + '/../client/dist'));
 app.use('/', express.static(__dirname + '/../client/dist'));
 
-app.get('/workspace-api/workspace/:id', async (req, res) => {
+app.get('/workspace-api/workspace/:id', async function(req, res) {
   const { id } = req.params;
   const { data } = await axios.get(`http://localhost:4001/workspace-api/workspace/${id}`);
   res.json(data);
 });
 
-app.get('/api/availability', async (req, res) => {
+app.get('/api/availability', async function(req, res) {
   const { id } = req.query;
   const { data } = await axios.get(`http://localhost:3001/api/availability?id=${id}`);
   res.json(data);
 });
+
+app.get('/api/workspace-description/:id', async function(req, res) {
+  const { id } = req.params;
+  const { data } = await axios.get(`http://localhost:6060/api/workspace-description/${id}`);
+  res.json(data);
+})
 
 app.get('/favicon.ico', (req, res) => res.end());
 
