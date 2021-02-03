ecea3430ef749c78576e8989a1b25782817babc9
Benntobox <benjaminsboyle3@gmail.com>
2021-02-02 22:15:44 -0800

    Adding connection for contact-service

--- server/server.js
+++ server/server.js
@@ -8,7 +8,13 @@ app.use('/', express.static(__dirname + '/../client/dist'));
 
 app.get('/workspace-api/workspace/:id', async (req, res) => {
   const { id } = req.params;
-  const { data } = await axios.get(`http://localhost:4000/workspace-api/workspace/${id}`);
+  const { data } = await axios.get(`http://localhost:4001/workspace-api/workspace/${id}`);
+  res.json(data);
+});
+
+app.get('/api/availability', async (req, res) => {
+  const { id } = req.query;
+  const { data } = await axios.get(`http://localhost:3001/api/availability?id=${id}`);
   res.json(data);
 });
 
