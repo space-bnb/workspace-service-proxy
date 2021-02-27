e5b2876f887b4a5e233ec732b5aa976b1eef0857
Benntobox <benjaminsboyle3@gmail.com>
2021-02-27 10:59:31 -0800

    Final cleanup for proxy

--- server/server.js
+++ server/server.js
@@ -8,62 +8,122 @@ app.use('/', express.static(__dirname + '/../client/dist'));
 
 app.get('/workspace-api/workspace/:id', async function(req, res) {
   const { id } = req.params;
-  const { data } = await axios.get(`http://54.193.132.156:4000/workspace-api/workspace/${id}`);
-  res.json(data);
+  try {
+    const { data } = await axios.get(`http://54.193.132.156:4000/workspace-api/workspace/${id}`);
+    res.json(data);
+  }
+  catch(err) {
+    console.log(err)
+    res.end();
+  }
 });
 
 app.get('/amenities-api/amenity/:id', async function(req, res) {
   const { id } = req.params;
-  const { data } = await axios.get(`http://54.219.57.231:4002/amenities-api/amenity/${id}`);
-  res.json(data);
+  try {
+    const { data } = await axios.get(`http://54.219.57.231:4002/amenities-api/amenity/${id}`);
+    res.json(data);
+  }
+  catch(err) {
+    console.log(err);
+    res.end();
+  }
 });
 
 app.get('/api/availability', async function(req, res) {
   const { id } = req.query;
-  const { data } = await axios.get(`http://3.140.156.174:3001/api/availability?id=${id}`);
-  res.json(data);
+  try {
+    const { data } = await axios.get(`http://3.140.156.174:3001/api/availability?id=${id}`);
+    res.json(data);
+  }
+  catch(err) {
+    console.log(err);
+    res.end();
+  }
 });
 
 app.get('/api/getNearbyTransitOptions/:id', async function(req, res) {
   const { id } = req.params;
-  const { data } = await axios.get(`http://3.140.156.174:3002/api/availability?id=${id}`);
-  res.json(data);
+  try {
+    const { data } = await axios.get(`http://3.140.156.174:3001/api/getNearbyTransitOptions/${id}`);
+    res.json(data);
+  }
+  catch(err) {
+    console.log(err);
+    res.end();
+  }
 });
 
 app.get('/api/photos/:id', async function(req, res) {
   const { id } = req.params;
-  const { data } = await axios.get(`http://54.151.43.93:6001/api/photos/${id}`);
-  res.json(data);
+  try {
+    const { data } = await axios.get(`http://54.151.43.93:6001/api/photos/${id}`);
+    res.json(data);
+  }
+  catch(err) {
+    console.log(err);
+    res.end();
+  }
 });
 
 app.get('/api/photos/workspace/:id', async function(req, res) {
   const { id } = req.params;
-  const { data } = await axios.get(`http://54.151.43.93:6001/api/photos/workspace/${id}`);
-  res.json(data);
+  try {
+    const { data } = await axios.get(`http://54.151.43.93:6001/api/photos/workspace/${id}`);
+    res.json(data);
+  }
+  catch(err) {
+    console.log(err);
+    res.end();
+  }
 });
 
 app.get('/api/workspace-description/:id', async function(req, res) {
   const { id } = req.params;
-  const { data } = await axios.get(`http://54.151.43.93:6060/api/workspace-description/${id}`);
-  res.json(data);
+  try {
+    const { data } = await axios.get(`http://54.151.43.93:6060/api/workspace-description/${id}`);
+    res.json(data);
+  }
+  catch(err) {
+    console.log(err);
+    res.end();
+  }
 });
 
 app.get('/api/nearbyworkspaces/:id', async function(req, res) {
   const { id } = req.params;
-  const { data } = await axios.get(`http://ec2-54-177-170-134.us-west-1.compute.amazonaws.com:5001/api/nearbyworkspaces/${id}`);
-  res.json(data);
+  try {
+    const { data } = await axios.get(`http://ec2-54-177-170-134.us-west-1.compute.amazonaws.com:5001/api/nearbyworkspaces/${id}`);
+    res.json(data);
+  }
+  catch(err) {
+    console.log(err);
+    res.end();
+  }
 });
 
 app.get('/api/nearbyworkspaces/address/:id', async function(req, res) {
   const { id } = req.params;
-  const { data } = await axios.get(`http://ec2-54-177-170-134.us-west-1.compute.amazonaws.com:5001/api/nearbyworkspaces/address/${id}`);
-  res.json(data);
+  try {
+    const { data } = await axios.get(`http://ec2-54-177-170-134.us-west-1.compute.amazonaws.com:5001/api/nearbyworkspaces/address/${id}`);
+    res.json(data);
+  }
+  catch(err) {
+    console.log(err);
+    res.end();
+  }
 });
 
 app.get('/api/nearbyworkspaces/buildings/:id', async function(req, res) {
   const { id } = req.params;
-  const { data } = await axios.get(`http://ec2-54-177-170-134.us-west-1.compute.amazonaws.com:5001/api/nearbyworkspaces/buildings/${id}`);
-  res.json(data);
+  try {
+    const { data } = await axios.get(`http://ec2-54-177-170-134.us-west-1.compute.amazonaws.com:5001/api/nearbyworkspaces/buildings/${id}`);
+    res.json(data);
+  }
+  catch(err) {
+    console.log(err);
+    res.end();
+  }
 });
 
 app.get('/favicon.ico', (req, res) => res.end());
